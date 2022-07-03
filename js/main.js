d4cfeca084d1b2471cdfaaf306f57e4c6c3a7013
webazex <webazex@gmail.com>
2022-07-04 01:38:08 +0300

    step two

--- js/main.js
+++ js/main.js
@@ -3,13 +3,21 @@ $(document).ready(function (){
 
    //change bg header for scrolling
    var scrolled;
+   let patchToLogo = $('.header-container__logo').attr('data-patch');
    window.onscroll = function() {
       scrolled = window.pageYOffset || document.documentElement.scrollTop;
       if(scrolled > 100){
-         $("header").css({"background-color": "white"})
+         $("header").css({"background-color": "white"});
+         $('.site-size__header-container .link__text').css({'color': '#232323'});
+         $('.contact-box__text-desc').css({'color': '#E50746'});
+         $('.contact-box__text-desc').css({'text-decoration': 'none'});
+         $('.header-container__logo').attr('src', patchToLogo+'lb.png');
       }
       if(scrolled < 100){
-         $("header").css({"background-color": "transparent"})
+         $("header").css({"background-color": "transparent"});
+         $('.site-size__header-container .link__text, .contact-box__text-desc').css({'color': 'white'});
+         $('.contact-box__text-desc').css({'text-decoration': 'underline'});
+         $('.header-container__logo').attr('src', patchToLogo+'lw.png');
       }
 
    }
